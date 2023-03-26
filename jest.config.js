ca153e2dc8a1fb6e1fcbeeec125acfb37cf4bc03
Tamika Nomara <taminomara@gmail.com>
2023-03-27 01:36:40 +0200

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.ts",
+    "!dist/**",
+    "!lib/**",
+    "!**/node_modules/**",
+  ],
+  coverageDirectory: "./coverage/",
+  moduleFileExtensions: ["js", "ts"],
+  reporters: ["default", "github-actions"],
+  testEnvironment: "node",
+  testMatch: ["**/*.test.ts"],
+  testRunner: "jest-circus/runner",
+  transform: {
+    "^.+\\.ts$": ["ts-jest", { diagnostics: false }],
+  },
+  verbose: true,
+};
+
+// This will strip :: commands from the log output during test runs
+
+const processStdoutWrite = process.stdout.write.bind(process.stdout);
+
+process.stdout.write = (str, encoding, cb) => {
+  if (!str.match(/^::/)) {
+    return processStdoutWrite(str, encoding, cb);
+  }
+};

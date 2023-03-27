7bdaeb7dcc8a315da436fdd8687a10c167efc09d
Tamika Nomara <taminomara@gmail.com>
2023-03-27 12:11:08 +0400

    Initial implementation

--- README.md
+++ README.md
@@ -1,22 +1,6 @@
 # GitHub Actions Problem Matcher
 
-[![CI](https://github.com/xt0rted/problem-matcher/actions/workflows/ci.yml/badge.svg?branch=main)](https://github.com/xt0rted/problem-matcher/actions/workflows/ci.yml)
-[![CodeQL](https://github.com/xt0rted/problem-matcher/actions/workflows/codeql-analysis.yml/badge.svg?branch=main)](https://github.com/xt0rted/problem-matcher/actions/workflows/codeql-analysis.yml)
-
-A GitHub Actions template to easily add or remove a problem matcher to workflows.
-
-## Setup
-
-This template has been designed so the only code change you should have to make is in [problem-matcher.json](.github/problem-matcher.json) and optionally the tests for it.
-When editing the `problem-matcher.json` you should set the owner name to the application and/or report style being matched.
-Examples of this are `eslint`, `eslint-compact`, or `eslint-stylish`.
-
-Make sure to also update the `action.yml` and `package.json` files.
-
-Sometimes report messages might not include a line or column number and your regular expression doesn't pick those lines up.
-To help avoid those situations tests are provided to give coverage to the regular expression.
-
-A useful resource to view and test your regular expression is [regex101.com](https://regex101.com/).
+Attaches a problem matcher that looks for errors during Sphinx builds.
 
 ## Usage
 
@@ -26,13 +10,9 @@ jobs:
   build:
     runs-on: ubuntu-latest
     steps:
-      - uses: actions/checkout@v2
-      - uses: xt0rted/problem-matcher@v1
-      - run: echo "Run some tests that will be picked up"
-      - uses: xt0rted/problem-matcher@v1
-        with:
-          action: remove
-      - run: echo "Run some more tests that shouldn't be picked up"
+      - uses: actions/checkout@v3
+      - uses: taminomara/sphinx-problem-matcher@v1
+      - run: cd docs && make html
 ```
 
 ## Options
--- __tests__/problemMatcher.test.ts
+++ __tests__/problemMatcher.test.ts
@@ -1,63 +1,117 @@
-import { matchResults } from "../__helpers__/utils";
 import { problemMatcher as problemMatcherJson } from "../src/problem-matcher.json";
 
 import type { ProblemMatcher, ProblemPattern } from "github-actions-problem-matcher-typings";
+import assert from "assert";
 
-const problemMatcher: ProblemMatcher = problemMatcherJson[0];
+const problemMatchers: ProblemMatcher[] = problemMatcherJson;
 
-describe("problemMatcher", () => {
-  it("has the correct owner", () => {
-    expect(problemMatcher.owner).toEqual("someLinter");
+function match(line: string, problemPattern: ProblemPattern) {
+  const regexp = new RegExp(problemPattern.regexp);
+  const result = regexp.exec(line);
+  if (result !== null) {
+    return {
+      file: problemPattern.file !== undefined ? result[problemPattern.file] : undefined,
+      line: problemPattern.line !== undefined ? result[problemPattern.line] : undefined,
+      column: problemPattern.column !== undefined ? result[problemPattern.column] : undefined,
+      code: problemPattern.code !== undefined ? result[problemPattern.code] : undefined,
+      message: problemPattern.message !== undefined ? result[problemPattern.message] : undefined,
+      severity: problemPattern.severity !== undefined ? result[problemPattern.severity] : undefined,
+    }
+  }
+}
+
+const testCases = [
+  {
+    desc: "full matcher",
+    log: "/Users/taminomara/Documents/yuio/docs/source/app.rst:6: ERROR: Unknown directive type \"xxx\".",
+    matcher: problemMatchers[0],
+    file: "/Users/taminomara/Documents/yuio/docs/source/app.rst",
+    line: "6",
+    message: "Unknown directive type \"xxx\".",
+    severity: "ERROR",
+  },
+  {
+    desc: "autodoc matcher",
+    log: "/opt/hostedtoolcache/Python/3.8.16/x64/lib/python3.8/site-packages/yuio/app.py:docstring of yuio.app.App.SubCommand:1: WARNING: undefined label: 'foobar'",
+    matcher: problemMatchers[1],
+    file: "/opt/hostedtoolcache/Python/3.8.16/x64/lib/python3.8/site-packages/yuio/app.py",
+    message: "docstring of yuio.app.App.SubCommand:1: WARNING: undefined label: 'foobar'",
+    severity: "WARNING",
+  },
+  {
+    desc: "loose matcher",
+    log: "checking consistency... /Users/taminomara/Documents/yuio/docs/source/app.rst: WARNING: document isn't included in any toctree",
+    matcher: problemMatchers[2],
+    file: "/Users/taminomara/Documents/yuio/docs/source/app.rst",
+    message: "document isn't included in any toctree",
+    severity: "WARNING",
+  },
+  {
+    desc: "loose matcher 2",
+    log: "WARNING: dot command 'dot' cannot be run (needed for graphviz output), check the graphviz_dot setting",
+    matcher: problemMatchers[3],
+    message: "dot command 'dot' cannot be run (needed for graphviz output), check the graphviz_dot setting",
+    severity: "WARNING",
+  },
+];
+
+
+describe("pattern matchers", () => {
+  it("has correct number of pattern matchers", () => {
+    expect(problemMatchers.length).toEqual(4);
   });
 
-  it("has one pattern", () => {
-    expect(problemMatcher.pattern.length).toEqual(1);
-  });
+  const seenOwners: string[] = [];
 
-  describe("pattern", () => {
-    const reportOutput = [
-      "README.md:12:81 MD013/line-length Line length [Expected: 80; Actual: 149]",
-      "docs/README.md:21:81 MD013/line-length Line length [Expected: 80; Actual: 119]",
-      "docs/README.md:14 MD012/no-multiple-blanks Multiple consecutive blank lines [Expected: 1; Actual: 2]",
-    ];
+  for (const i in problemMatchers) {
+    const problemMatcher = problemMatchers[i];
+    describe(`problem matcher ${i}`, () => {
+      it("has one pattern", () => {
+        expect(problemMatcher.pattern.length).toEqual(1);
+      });
 
-    let pattern: ProblemPattern;
-    let results: RegExpExecArray[];
-
-    beforeEach(() => {
-      pattern = problemMatcher.pattern[0];
-
-      const regexp = new RegExp(pattern.regexp);
-
-      results = matchResults(reportOutput, regexp);
+      it("has the correct owner", () => {
+        const prefix = "sphinx-problem-matcher";
+        expect(problemMatcher.owner.substring(0, prefix.length)).toEqual(prefix);
+      });
+      it("has a unique owner", () => {
+        expect(seenOwners).not.toContain(problemMatcher.owner);
+        seenOwners.push(problemMatcher.owner);
+      });
     });
-
-    it("matches violations", () => {
-      expect(results.length).toEqual(3);
-    });
-
-    it("matches violation details", () => {
-      expect(results[0][pattern.file]).toEqual("README.md");
-      expect(results[0][pattern.line]).toEqual("12");
-      expect(results[0][pattern.column]).toEqual("81");
-      expect(results[0][pattern.code]).toEqual("MD013/line-length");
-      expect(results[0][pattern.message]).toEqual("Line length [Expected: 80; Actual: 149]");
-    });
-
-    it("matches violation details in sub folder", () => {
-      expect(results[1][pattern.file]).toEqual("docs/README.md");
-      expect(results[1][pattern.line]).toEqual("21");
-      expect(results[1][pattern.column]).toEqual("81");
-      expect(results[1][pattern.code]).toEqual("MD013/line-length");
-      expect(results[1][pattern.message]).toEqual("Line length [Expected: 80; Actual: 119]");
-    });
-
-    it("matches violation details without column", () => {
-      expect(results[2][pattern.file]).toEqual("docs/README.md");
-      expect(results[2][pattern.line]).toEqual("14");
-      expect(results[2][pattern.column]).toBeUndefined();
-      expect(results[2][pattern.code]).toEqual("MD012/no-multiple-blanks");
-      expect(results[2][pattern.message]).toEqual("Multiple consecutive blank lines [Expected: 1; Actual: 2]");
-    });
-  });
+  }
 });
+
+for (const testCase of testCases) {
+  const {desc, log, matcher, file, line, message, severity} = testCase;
+  const pattern = matcher.pattern[0];
+
+describe(testCase.desc, () => {
+    const result = match(log, pattern);
+
+    it ("has one pattern", () => {
+      expect(matcher.pattern.length).toEqual(1);
+    });
+
+    it("matches an error", () => {
+      expect(result).not.toBeNaN();
+      expect(result).not.toBeUndefined();
+    });
+
+    it ("returns correct file", () => {
+      expect((result || {}).file).toEqual(file);
+    });
+
+    it ("returns correct line", () => {
+      expect((result || {}).line).toEqual(line);
+    });
+
+    it ("returns correct message", () => {
+      expect((result || {}).message).toEqual(message);
+    });
+
+    it ("returns correct severity", () => {
+      expect((result || {}).severity).toEqual(severity);
+    });
+  });
+}
